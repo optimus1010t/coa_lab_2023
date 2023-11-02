fc36f999d2353b62b6c577e4659b6c33ac635909
optimus1010t <tanishqprasad1003@gmail.com>
2023-11-02 20:11:35 +0530

    added file reading part

--- assgn_7/mem/mem_mod.v
+++ assgn_7/mem/mem_mod.v
@@ -5,15 +5,14 @@ module data_mem_mod(
     input [31:0] write_data,                  // data to write to register in memory bank
     output reg [31:0] read_data
 );
-    reg [31:0] registers [1023:0];            // 1024 registers that act as memory of width 32 bits                                            
+    reg [31:0] data_regs [1023:0];            // 1024 registers that act as memory of width 32 bits                                            
 
     initial begin
-        // get the values from .txt file using mem add functionale that is only synthesisable in case of initial blocks
-        // ???? need to add the code for initialising the mem bank
+        $readmemb("rams_init_file_data.data",data_regs); // The external file that is used to initialize the RAM needs to be in bit vector form. External files in integer or hex format will not work.
     end
     always @(*) begin
     if (memRead) begin                        // read ports are always updated based on the address of the source registers    
-        read_data = registers[sr];            // read_data1 is updated based on the address of the source register
+        read_data = data_regs[sr];            // read_data1 is updated based on the address of the source register
     end
     end
     // some default values set for the registers
@@ -25,7 +24,7 @@ module data_mem_mod(
         end
         else begin                            
             if (memWrite)                     // write to register only when writeReg is high    
-                registers[sr] <= write_data;
+                data_regs[sr] <= write_data;
         end
     end
 endmodule
@@ -37,15 +36,14 @@ module instr_mem_mod(
     input [31:0] write_data,                  // data to write to register in memory bank
     output reg [31:0] read_data
 );
-    reg [31:0] registers [1023:0];            // 1024 registers that act as memory of width 32 bits                                            
+    reg [31:0] inst_regs [1023:0];            // 1024 registers that act as memory of width 32 bits                                            
 
     initial begin
-        // get the values from .txt file using mem add functionale that is only synthesisable in case of initial blocks
-        // ???? need to add the code for initialising the mem bank
+        $readmemb("rams_init_file_data.data",inst_regs);
     end
     always @(*) begin
     if (memRead) begin                        // read ports are always updated based on the address of the source registers    
-        read_data = registers[sr];            // read_data1 is updated based on the address of the source register
+        read_data = inst_regs[sr];            // read_data1 is updated based on the address of the source register
     end
     end
     // some default values set for the registers
@@ -57,7 +55,7 @@ module instr_mem_mod(
         end
         else begin                            
             if (memWrite)                     // write to register only when writeReg is high    
-                registers[sr] <= write_data;
+                inst_regs[sr] <= write_data;
         end
     end
 endmodule
